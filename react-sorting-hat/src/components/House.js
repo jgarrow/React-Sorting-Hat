0a32dce1bebaae7e7207815ea61f220a1ffd7c1c
Janessa Garrow <janessa.garrow@gmail.com>
2020-01-16 21:41:50 -0700

    Create data fixture with house info. Render house info to screen for house you were sorted into

--- react-sorting-hat/src/components/House.js
+++ react-sorting-hat/src/components/House.js
@@ -1,3 +1,5 @@
+/** @jsx jsx */
+import { css, jsx } from "@emotion/core";
 import React, { Component } from "react";
 import styled from "@emotion/styled";
 
@@ -8,6 +10,66 @@ const OuterContainer = styled.div`
     position: relative;
     display: inline-block;
 `;
+
+const InnerContainer = styled.div`
+    width: 80%;
+    margin: 0 auto;
+`;
+
+const houseInfo = {
+    hufflepuff: {
+        name: "hufflepuff",
+        crest:
+            "https://vignette.wikia.nocookie.net/pottermore/images/5/5e/Hufflepuff_crest.png/revision/latest/scale-to-width-down/180?cb=20111112232427",
+        founder: "Helga Hufflepuff",
+        animal: "Badger",
+        colors: "Yellow and black",
+        traits: "Hard work, patience, loyalty and fair play",
+        common_room: "Hufflepuff Basement",
+        head: "Pomona Sprout",
+        ghost: "Fat Friar",
+        famous_people: "Newt Scamander"
+    },
+    ravenclaw: {
+        name: "ravenclaw",
+        crest:
+            "https://vignette.wikia.nocookie.net/pottermore/images/4/40/Ravenclaw_Crest_1.png/revision/latest/scale-to-width-down/180?cb=20140604194505",
+        founder: "Rowena Ravenclaw",
+        animal: "Eagle",
+        colors: "Blue and bronze",
+        traits: "Wit, intelligence, creativity and wisdom",
+        common_room: "Ravenclaw Tower",
+        head: "Filius Flitwick",
+        ghost: "Grey Lady",
+        famous_people: "Garrick Ollivander, Filius Flitwick, Gilderoy Lockhart"
+    },
+    gryffindor: {
+        name: "gryffindor",
+        crest:
+            "https://vignette.wikia.nocookie.net/pottermore/images/1/16/Gryffindor_crest.png/revision/latest/scale-to-width-down/180?cb=20111112232412",
+        founder: "Godric Gryffindor",
+        animal: "Lion",
+        colors: "Red and gold",
+        traits: "Daring, nerve, and chivalry",
+        common_room: "Gryffindor Tower",
+        head: "Minerva McGonagall",
+        ghost: "Nearly-Headless Nick",
+        famous_people: "Minerva McGonagall, "
+    },
+    slytherin: {
+        name: "slytherin",
+        crest:
+            "https://vignette.wikia.nocookie.net/pottermore/images/4/45/Slytherin_Crest.png/revision/latest/scale-to-width-down/180?cb=20111112232353",
+        founder: "Salazar Slytherin",
+        animal: "Snake",
+        colors: "Green and silver",
+        traits: "Cunning, resourcefulness and ambition",
+        common_room: "Slytherin Dungeon",
+        head: "Severus Snape",
+        ghost: "Bloody Baron",
+        famous_people: "Merlin, Tom Riddle, Severus Snape"
+    }
+};
 
 class House extends Component {
     constructor(props) {
@@ -28,6 +90,8 @@ class House extends Component {
 
         console.log("houses: ", houses);
 
+        // handles getting key ("house") with most points
+        // TODO: handle if there's a tie --> want to pick randomly between houses that are tied for most points
         yourHouse = Object.keys(houses).reduce((a, b) =>
             houses[a] > houses[b] ? a : b
         );
@@ -39,7 +103,53 @@ class House extends Component {
     render() {
         return (
             <OuterContainer>
-                <h1>Congratulations! You are in {this.state.house}!</h1>
+                <InnerContainer>
+                    <h1 css={{ textAlign: "center" }}>
+                        Congratulations!
+                        <br />
+                        You are in {this.state.house}!
+                    </h1>
+                    {this.state.house !== "" && (
+                        <div
+                            css={{
+                                width: "100%",
+                                display: "flex",
+                                flexFlow: "column nowrap",
+                                justifyContent: "center",
+                                alignItems: "center"
+                            }}
+                        >
+                            <img
+                                css={{
+                                    margin: "0 auto"
+                                }}
+                                src={houseInfo[this.state.house].crest}
+                                alt={`${this.state.house} crest`}
+                            />
+                            <p>
+                                Founder: {houseInfo[this.state.house].founder}
+                            </p>
+                            <p>Animal: {houseInfo[this.state.house].animal}</p>
+                            <p>Colors: {houseInfo[this.state.house].colors}</p>
+                            <p>Traits: {houseInfo[this.state.house].traits}</p>
+                            <p>
+                                Common Room:{" "}
+                                {houseInfo[this.state.house]["common_room"]}
+                            </p>
+                            <p>
+                                Head of House:{" "}
+                                {houseInfo[this.state.house].head}
+                            </p>
+                            <p>
+                                House Ghost: {houseInfo[this.state.house].ghost}
+                            </p>
+                            <p>
+                                Famous People:{" "}
+                                {houseInfo[this.state.house]["famous_people"]}
+                            </p>
+                        </div>
+                    )}
+                </InnerContainer>
             </OuterContainer>
         );
     }
