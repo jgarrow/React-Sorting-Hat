6354f1e69c0e52913aab36fb22ad5c7d39374acb
Janessa Garrow <janessa.garrow@gmail.com>
2020-01-17 22:51:31 -0700

    Add sorting hat image to Link

--- react-sorting-hat/src/components/Welcome.js
+++ react-sorting-hat/src/components/Welcome.js
@@ -1,20 +1,25 @@
+/** @jsx jsx */
+import { css, jsx } from "@emotion/core";
 import React from "react";
 import styled from "@emotion/styled";
 import { Link } from "react-router-dom";
+
+import SortingHat from "../img/sorting-hat.png";
 
 const Heading = styled.h1`
     font-family: "Harry Potter";
     font-size: 3rem;
     margin-top: 5rem;
+    margin-bottom: 0;
 `;
 
 const Button = styled(Link)`
     display: inline-block;
     cursor: pointer;
     box-sizing: border-box;
-    padding: 5px 10px;
+    padding: 10px 15px;
     text-align: center;
-    background: lightgray;
+    background: #faebd7;
     border-radius: 5px;
     text-decoration: none;
     color: black;
@@ -24,6 +29,23 @@ const Welcome = () => {
     return (
         <>
             <Heading>Welcome to Hogwarts</Heading>
+            <Link
+                to="/sorting-ceremony"
+                css={{
+                    cursor: "pointer",
+                    textDecoration: "none",
+                    background: "transparent"
+                }}
+            >
+                <img
+                    src={SortingHat}
+                    alt="Sorting Hat"
+                    css={{
+                        width: "200px",
+                        height: "auto"
+                    }}
+                />
+            </Link>
             <Button to="/sorting-ceremony">Begin Sorting Ceremony</Button>
         </>
     );
